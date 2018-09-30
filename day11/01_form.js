501cba5f56d25cd5f6f6054496dac6d575f8f3ba
wen <12@qq.com>
2018-09-30 12:44:25 +0800

    第三次提交

--- day11/01_form.js
+++ day11/01_form.js
@@ -2,6 +2,7 @@ var http = require('http');
 var fs=require("fs");
 // 引用formidable模块
 var fd=require("formidable");
+var sd=require("silly-datetime");
 var server = http.createServer(function (req, res) {
   if (req.url == '/favicon.ico') {
     return;
@@ -19,7 +20,9 @@ var server = http.createServer(function (req, res) {
     // post的/tijiao请求
     // 使用formidable模块处理请求
     // 创建表单对象
-    var form=new fd.IncomingForm();
+    var form=fd.IncomingForm();
+    // 设置上传文件保存的路径
+    form.uploadDir="./uploads";
     // 解析请求对象
     // fields:解析得到的文本域的参数值
     form.parse(req,function(err,fields,files){
@@ -32,7 +35,26 @@ var server = http.createServer(function (req, res) {
       console.log(fields);
       console.log('files');
       console.log(files);
-      res.end("请求成功");
+      // 对文件改名
+      // 获取旧路径
+      var oldPath=files.pic.path;
+      // 设置新路径
+      var newName=files.pic.name;//获取名称
+      var arr=newName.split('.');
+      var ext=arr[arr.length-1];//获取后缀名
+      var name=sd.format(new Date(),"YYYYMMDDHHmmss");
+      // 上传文件的新名称
+      var newName1=name+"."+ext;
+      // 新路径
+      var newPath="./uploads/"+newName1;
+      fs.rename(oldPath,newPath,function(err){
+        if(err){
+          console.log(err);
+          res.end("重命名失败");
+          return;
+        }
+        res.end("请求成功");
+      })
     })
   }else{
     res.end("地址错误");
