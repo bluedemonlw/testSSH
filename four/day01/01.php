24ce56929967a9e5cb36aa32a2b1c22e7efa591d
wen <12@qq.com>
2018-10-22 17:45:08 +0800

    原生的Ajax

---
+++ four/day01/01.php
@@ -0,0 +1,6 @@
+<?php
+// 使用超全局变量$_GET
+$user=$_POST['name'];
+$pwd=$_POST['pwd'];
+echo '用户名'.$user.'密码'.$pwd;
+?>
---
+++ four/day01/02.php
@@ -0,0 +1,6 @@
+<?php
+// 获取first.json
+// 使用file_get)contents(文件路径)
+$data=file_get_contents('./first.json');
+echo $data;
+?>
